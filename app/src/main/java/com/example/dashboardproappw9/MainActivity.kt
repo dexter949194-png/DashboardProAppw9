2774af60fe3892ca5ce1716ecbc8b44f4f836850
dexter949194-png <dexter.949194@gmail.com>
2026-02-14 13:11:49 +0100

    Update MainActivity.kt

--- app/src/main/java/com/example/dashboardproappw9/MainActivity.kt
+++ app/src/main/java/com/example/dashboardproappw9/MainActivity.kt
@@ -3,33 +3,24 @@ package com.example.dashboardproappw9
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
-        // Global crash handler
-        Thread.setDefaultUncaughtExceptionHandler { thread, throwable ->
-            val intent = Intent(this, CrashActivity::class.java)
-            intent.putExtra("error", throwable.toString() + "\n\n" + throwable.stackTrace.joinToString("\n"))
-            startActivity(intent)
-            finish()
-        }
-
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Prosty UI i przycisk do skanowania
-        val btnScan = findViewById<Button>(R.id.btnScan)
-        btnScan.setOnClickListener {
-            try {
+        try {
+            val btnScan = findViewById<Button>(R.id.btnScan)
+            btnScan?.setOnClickListener {
                 val intent = Intent(this, ScanActivity::class.java)
                 startActivity(intent)
-            } catch (e: Exception) {
-                throw e
             }
+        } catch (e: Exception) {
+            Toast.makeText(this, "Brak przycisku Scan w layout!", Toast.LENGTH_LONG).show()
         }
     }
 }
