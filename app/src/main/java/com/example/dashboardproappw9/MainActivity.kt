ddc46090d2c2de37a3a7fcc0e88ee4d24cee5637
dexter949194-png <dexter.949194@gmail.com>
2026-02-14 18:48:55 +0100

    Update MainActivity.kt

--- app/src/main/java/com/example/dashboardproappw9/MainActivity.kt
+++ app/src/main/java/com/example/dashboardproappw9/MainActivity.kt
@@ -7,7 +7,6 @@ import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
