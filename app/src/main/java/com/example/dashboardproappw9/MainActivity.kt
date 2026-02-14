1b453fea30cd7e7e9bdcb6e54ecf50e943d6d0e4
dexter949194-png <dexter.949194@gmail.com>
2026-02-14 14:31:24 +0100

    Update MainActivity.kt

--- app/src/main/java/com/example/dashboardproappw9/MainActivity.kt
+++ app/src/main/java/com/example/dashboardproappw9/MainActivity.kt
@@ -6,11 +6,29 @@ import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
 
-        val btn = Button(this)
-        btn.text = "Skanuj BLE"
+        // Crash handler
+        Thread.setDefaultUncaughtExceptionHandler { _, throwable ->
+            val intent = Intent(this, CrashActivity::class.java)
+            intent.putExtra("error", buildString {
+                appendLine("Exception: ${throwable.javaClass.simpleName}")
+                appendLine("Message: ${throwable.message}")
+                appendLine()
+                appendLine("Stacktrace:")
+                throwable.stackTrace.forEach {
+                    appendLine(it.toString())
+                }
+            })
+            startActivity(intent)
+            finish()
+        }
+
+        // Button to trigger next
+        val btn = Button(this).apply { text = "Start Scan" }
         btn.setOnClickListener {
             startActivity(Intent(this, ScanActivity::class.java))
         }
