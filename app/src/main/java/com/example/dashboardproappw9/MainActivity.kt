5aaca0595344da7fc1e71d65594226a386f4c109
dexter949194-png <dexter.949194@gmail.com>
2026-02-14 13:26:08 +0100

    Update MainActivity.kt

--- app/src/main/java/com/example/dashboardproappw9/MainActivity.kt
+++ app/src/main/java/com/example/dashboardproappw9/MainActivity.kt
@@ -3,24 +3,17 @@ package com.example.dashboardproappw9
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
-
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        try {
-            val btnScan = findViewById<Button>(R.id.btnScan)
-            btnScan?.setOnClickListener {
-                val intent = Intent(this, ScanActivity::class.java)
-                startActivity(intent)
-            }
-        } catch (e: Exception) {
-            Toast.makeText(this, "Brak przycisku Scan w layout!", Toast.LENGTH_LONG).show()
+        val btn = Button(this)
+        btn.text = "Skanuj BLE"
+        btn.setOnClickListener {
+            startActivity(Intent(this, ScanActivity::class.java))
         }
+        setContentView(btn)
     }
 }
