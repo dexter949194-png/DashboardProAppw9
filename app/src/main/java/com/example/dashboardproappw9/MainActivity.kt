f34c138f0d0d292ad491576d282a9a839cae9476
dexter949194-png <dexter.949194@gmail.com>
2026-02-14 13:02:44 +0100

    Update MainActivity.kt

--- app/src/main/java/com/example/dashboardproappw9/MainActivity.kt
+++ app/src/main/java/com/example/dashboardproappw9/MainActivity.kt
@@ -1,11 +1,35 @@
 package com.example.dashboardproappw9
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        // Global crash handler
+        Thread.setDefaultUncaughtExceptionHandler { thread, throwable ->
+            val intent = Intent(this, CrashActivity::class.java)
+            intent.putExtra("error", throwable.toString() + "\n\n" + throwable.stackTrace.joinToString("\n"))
+            startActivity(intent)
+            finish()
+        }
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Prosty UI i przycisk do skanowania
+        val btnScan = findViewById<Button>(R.id.btnScan)
+        btnScan.setOnClickListener {
+            try {
+                val intent = Intent(this, ScanActivity::class.java)
+                startActivity(intent)
+            } catch (e: Exception) {
+                throw e
+            }
+        }
     }
 }
