7b6e32856af8cf8a2ad123bf155be281d45fc7c5
dexter949194-png <dexter.949194@gmail.com>
2026-02-14 13:27:26 +0100

    Update ScanActivity.kt

--- app/src/main/java/com/example/dashboardproappw9/ScanActivity.kt
+++ app/src/main/java/com/example/dashboardproappw9/ScanActivity.kt
@@ -1,1 +1,9 @@
-app/src/main/java/com/example/dashboardproappw9/MainActivity.kt
+class ScanActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val btn = Button(this)
+        btn.text = "Powrót"
+        btn.setOnClickListener { finish() }
+        setContentView(btn)
+    }
+}
